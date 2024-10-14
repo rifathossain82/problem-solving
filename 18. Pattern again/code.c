981b659e6dccc8d997ae21b5216f289fb0c60681
rifathossain82 <officialrifat82@gmail.com>
2024-10-14 19:43:30 +0600

    add: Pattern again

---
+++ 18. Pattern again/code.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    for (int i = n, p = 1; i >= 1, p <= n; i--, p++)
+    {
+        for (int k = 1; k <= p; k++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j < i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
