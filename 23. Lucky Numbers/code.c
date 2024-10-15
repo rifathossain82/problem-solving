e40fe349e8e941e5931148d5e2e2649197e66ab9
rifathossain82 <officialrifat82@gmail.com>
2024-10-15 11:23:39 +0600

    add: Lucky Numbers

---
+++ 23. Lucky Numbers/code.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int isLuckyNumber(int n)
+{
+    int temp = n, isLucky = 1;
+    while (temp != 0)
+    {
+        int digit = temp % 10;
+        temp = temp / 10;
+
+        if (digit == 4 || digit == 7)
+        {
+            // Nothing to do.
+        }
+        else
+        {
+            isLucky = 0;
+        }
+    }
+
+    return isLucky;
+}
+
+int main()
+{
+    int a, b, isNoLucky = 1;
+    scanf("%d %d", &a, &b);
+
+    for (int i = a; i <= b; i++)
+    {
+        if (isLuckyNumber(i) == 1)
+        {
+            printf("%d ", i);
+            isNoLucky = 0;
+        }
+    }
+
+    if (isNoLucky == 1)
+    {
+        printf("-1");
+    }
+
+    return 0;
+}
