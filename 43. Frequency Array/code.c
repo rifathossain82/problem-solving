95507b8ff31ae63993f5e78148da7423c382a06b
rifathossain82 <officialrifat82@gmail.com>
2024-10-20 16:45:36 +0600

    add: Frequency Array

---
+++ 43. Frequency Array/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < m; i++)
+    {   
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] == arr[i])
+            {
+                count++;
+            }
+        }
+
+        printf("%d\n", count);
+    }
+
+    return 0;
+}
