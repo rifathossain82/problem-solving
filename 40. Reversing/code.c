926a6ddfd67bc59f02f30e530f1864d4b4bdc0c6
rifathossain82 <officialrifat82@gmail.com>
2024-10-20 10:22:07 +0600

    add: Reversing

---
+++ 40. Reversing/code.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // To take input:
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // To reversed the array:
+    for (int i = 0; i < n / 2; i++)
+    {
+        // Like number swaping
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    // To print the reversed arrary:
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
